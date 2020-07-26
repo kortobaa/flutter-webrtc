9d89e0ce7b14f786c30b20b1e36043f5e9672e74
Ayman-Barghout <ayman.a.barghout@gmail.com>
2020-07-26 12:37:15 +0200

    Fixed an issue with the Android side of the plugin that caused problems with bluetooth headsets.

--- android/src/main/java/com/cloudwebrtc/webrtc/FlutterWebRTCPlugin.java
+++ android/src/main/java/com/cloudwebrtc/webrtc/FlutterWebRTCPlugin.java
@@ -122,23 +122,7 @@ public class FlutterWebRTCPlugin implements MethodCallHandler {
                 .setVideoEncoderFactory(new DefaultVideoEncoderFactory(eglContext, false, true))
                 .setVideoDecoderFactory(new DefaultVideoDecoderFactory(eglContext))
                 .setAudioDeviceModule(audioDeviceModule).createPeerConnectionFactory();
-
-        rtcAudioManager = RTCAudioManager.create(registrar.context());
-        // Store existing audio settings and change audio mode to
-        // MODE_IN_COMMUNICATION for best possible VoIP performance.
-        Log.d(TAG, "Starting the audio manager...");
-        rtcAudioManager.start(new RTCAudioManager.AudioManagerEvents() {
-            // This method will be called each time the number of available audio
-            // devices has changed.
-            @Override
-            public void onAudioDeviceChanged(RTCAudioManager.AudioDevice audioDevice,
-                    Set<RTCAudioManager.AudioDevice> availableAudioDevices) {
-                onAudioManagerDevicesChanged(audioDevice, availableAudioDevices);
-            }
-        });
-        /*
-         * if (audioManager != null) { audioManager.stop(); audioManager = null; }
-         */
+        
     }
 
     private void startAudioManager() {
